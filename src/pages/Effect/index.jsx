cbb64d316a5e0afbf27ca40f072cae0734c1abd1
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2023-07-03 19:33:16 -0300

    useEffect

--- src/pages/Effect/index.jsx
+++ src/pages/Effect/index.jsx
@@ -1,13 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Head } from "../../components/Head";
+import styles from "./styles.module.css";
 
 export const Effect = () => {
-  useEffect(() => {}, []);
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/CauaRodrigues/hooks-studies/lista",
+    )
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+
   return (
     <>
       <Head title="useEffect" />
 
-      <section className="container"></section>
+      <section className="container">
+        <div className={styles.content}>
+          {books.length ? (
+            books.map(({ id, categoria, livros }) => (
+              <details key={id}>
+                <summary className={styles.summary}>{categoria}</summary>
+                <ul className={styles.ul}>
+                  {livros.map((title, index) => (
+                    <li key={index}>{title}</li>
+                  ))}
+                </ul>
+              </details>
+            ))
+          ) : (
+            <span>loading...</span>
+          )}
+        </div>
+      </section>
     </>
   );
 };
